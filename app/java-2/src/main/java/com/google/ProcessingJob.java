158bb14765c28305635618fafbce6953ced8c29f
Nim Jayawardena <nimjay@google.com>
2024-04-07 10:08:14 -0700

    Implement processRawData()

--- app/java-2/src/main/java/com/google/ProcessingJob.java
+++ app/java-2/src/main/java/com/google/ProcessingJob.java
@@ -1,12 +1,20 @@
 package com.google;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.Hashtable;
 
 public class ProcessingJob
 {
@@ -22,7 +30,9 @@ public class ProcessingJob
             "🟢 Start ProcessingJob with: %s, %s", RAW_DATA_BUCKET, PROCESSED_DATA_BUCKET)
         );
         try {
-            downloadRawData();
+            String tempDataFilePath = downloadRawData();
+            Dictionary<String, Integer> colorAgeLocationToCount = processRawData(tempDataFilePath);
+            writeProcessedData(colorAgeLocationToCount);
         } catch (Exception e) {
             System.out.println("ProcessingJob failed: " + e.getMessage());
         }
@@ -53,11 +63,42 @@ public class ProcessingJob
         return tempDataFile.getAbsolutePath();
     }
 
-    public static void processRawData() {
-        // TODO: Implement
+    public static Dictionary<String, Integer> processRawData(String tempDataFilePath) 
+        throws FileNotFoundException, IOException {
+        System.out.println("processRawData: start processing data");
+        Dictionary<String, Integer> colorAgeLocationToCount = new Hashtable<String, Integer>();
+        // Go through each row of the CSV file.
+        FileReader reader = new FileReader(tempDataFilePath);
+        CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT.withHeader());
+        for (CSVRecord record : csvParser) {
+            // Ignore rows where crucial columns are empty/unknown.
+            boolean shouldIgnoreRow = false;
+            for (String column : CSV_COLUMNS) {
+                String value = record.get(column);
+                if (value == null || value.isEmpty() || value.equals("?")) {
+                    shouldIgnoreRow = true;
+                }
+            }
+
+            // Bump count in "Color/Age/Location" dictionary.
+            if (!shouldIgnoreRow) {
+                String key = String.format(
+                    "%s/%s/%s",
+                    record.get("Primary Fur Color"),
+                    record.get("Age"),
+                    record.get("Location")
+                );
+                Integer currCount = colorAgeLocationToCount.get(key);
+                if (currCount == null) {
+                    currCount = 0;
+                }
+                colorAgeLocationToCount.put(key, currCount + 1);
+            }
+        }
+        return colorAgeLocationToCount;
     }
 
-    public static void writeProcessedData() {
+    public static void writeProcessedData(Dictionary<String, Integer> colorAgeLocationToCount) {
         // TODO: Implement
     }
 }
