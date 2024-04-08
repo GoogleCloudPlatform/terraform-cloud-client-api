916a7fafa131ba1beceb6dff4d8227b5e4e2da6e
Nim Jayawardena <nimjay@google.com>
2024-04-07 19:49:24 -0700

    Implement writeProcessedData() & throwErrorIfInvalidArgs()

--- app/java-2/src/main/java/com/google/ProcessingJob.java
+++ app/java-2/src/main/java/com/google/ProcessingJob.java
@@ -4,14 +4,20 @@ import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
+import com.google.gson.Gson;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Dictionary;
 import java.util.Enumeration;
 import java.util.Hashtable;
@@ -29,6 +35,7 @@ public class ProcessingJob
             String.format(
             "🟢 Start ProcessingJob with: %s, %s", RAW_DATA_BUCKET, PROCESSED_DATA_BUCKET)
         );
+        throwErrorIfInvalidArgs();
         try {
             String tempDataFilePath = downloadRawData();
             Dictionary<String, Integer> colorAgeLocationToCount = processRawData(tempDataFilePath);
@@ -38,15 +45,17 @@ public class ProcessingJob
         }
     }
 
-    public static String downloadRawData() throws IOException 
-    {
-        System.out.println("downloadRawData: start downloading data");
+    private static void throwErrorIfInvalidArgs() {
         if (RAW_DATA_BUCKET == null || RAW_DATA_BUCKET.isEmpty()) {
             throw new IllegalArgumentException("RAW_DATA_BUCKET required");
         }
         if (PROCESSED_DATA_BUCKET == null || PROCESSED_DATA_BUCKET.isEmpty()) {
             throw new IllegalArgumentException("PROCESSED_DATA_BUCKET required");
         }
+    }
+
+    public static String downloadRawData() throws IOException {
+        System.out.println("downloadRawData: start downloading data");
 
         // Create temporary folder and file
         Path tempDir = Files.createTempDirectory("rawData");
@@ -106,7 +115,28 @@ public class ProcessingJob
         return colorAgeLocationToCount;
     }
 
-    public static void writeProcessedData(Dictionary<String, Integer> colorAgeLocationToCount) {
-        // TODO: Implement
+    /**
+     * Uploads a JSON file (to a Google Cloud Storage bucket)
+     * for each combination of facets (for each color-age-location combination).
+     */
+    public static void writeProcessedData(Dictionary<String, Integer> colorAgeLocationToCount) 
+        throws UnsupportedEncodingException {
+        System.out.println("writeProcessedData: start writing");
+        Gson gson = new Gson();
+        Enumeration<String> keys = colorAgeLocationToCount.keys();
+        Storage storage = StorageOptions.getDefaultInstance().getService();
+        while (keys.hasMoreElements()) {
+            String key = keys.nextElement();
+            String jsonString = gson.toJson(colorAgeLocationToCount.get(key));
+            BlobId cloudStorageBlobId = BlobId.of(PROCESSED_DATA_BUCKET, key + "/data.json");
+            BlobInfo cloudStorageBlobInfo = BlobInfo
+                .newBuilder(cloudStorageBlobId)
+                .setContentType("application/json")
+                .build();
+            String utf8CharsetName = StandardCharsets.UTF_8.name();
+            byte[] jsonStringAsByes = jsonString.getBytes(utf8CharsetName);
+            storage.create(cloudStorageBlobInfo, jsonStringAsByes, 0, jsonStringAsByes.length);
+        }
+        System.out.println("writeProcessedData: wrote " + colorAgeLocationToCount.size() + " files");
     }
 }
