f73ea8331a9730476cbde87b33bdc4c4b9adb584
Katie McLaughlin <katie@glasnt.com>
2024-04-08 15:16:53 -0700

    debug: debug spec identifier

--- infra/test/integration/language_deployment/language_deployment_test.go
+++ infra/test/integration/language_deployment/language_deployment_test.go
@@ -64,7 +64,7 @@ func TestLanguageDeployment(t *testing.T) {
 		// Ensure processed files appear as they should in Cloud Storage
 		// Retrieve processed bucket from service envvar
 		process_job := gcloud.Run(t, fmt.Sprintf("run jobs describe %s", jobName), gcloudOps)
-		process_bucket := process_job.Get("spec.template.spec.containers[0].env[PROCESS_BUCKET_NAME].value")
+		process_bucket := process_job.Get("spec.template.spec.template.spec.containers[0].env[PROCESS_BUCKET_NAME].value")
 		bucket_objects := gcloud.Run(t,
 			fmt.Sprintf("storage objects list --exhaustive gs://%s/**/data.json", process_bucket),
 			gcloudOps).Get("name").Array()
