fa4d5c73689f4747eea59704cd3767e670c7e3dd
Nim Jayawardena <nimjay@google.com>
2024-04-18 09:55:42 -0400

    Rename CloudClientApiApplication

--- app/java-3/src/main/java/com/google/cloudclientapi/CensusApplication.java
+++ app/java-3/src/main/java/com/google/cloudclientapi/CensusApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CloudClientApiApplication {
+public class CensusApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CloudClientApiApplication.class, args);
+		SpringApplication.run(CensusApplication.class, args);
 	}
 
 }
