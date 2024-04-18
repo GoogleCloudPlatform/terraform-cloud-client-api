6948e15e38ca82be40bcb39e445f45d4abf63fbf
Nim Jayawardena <nimjay@google.com>
2024-04-18 15:39:18 -0400

    Use Logger in ProcessingJob

--- app/java/src/main/java/com/google/cloudclientapi/ProcessingJob.java
+++ app/java/src/main/java/com/google/cloudclientapi/ProcessingJob.java
@@ -31,14 +31,18 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Dictionary;
 import java.util.Enumeration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ProcessingJob {
+
   private static String RAW_DATA_BUCKET = System.getenv().get("RAW_DATA_BUCKET");
   private static String PROCESSED_DATA_BUCKET = System.getenv().get("PROCESSED_DATA_BUCKET");
   private static String RAW_DATA_FILE = "squirrels.csv";
+  private static final Logger logger = LoggerFactory.getLogger(CensusController.class);
 
   public static void main(String[] args) {
-    System.out.println(
+    logger.info(
         String.format(
             "🟢 Start ProcessingJob with: %s, %s", RAW_DATA_BUCKET, PROCESSED_DATA_BUCKET));
     throwErrorIfInvalidArgs();
@@ -48,7 +52,7 @@ public class ProcessingJob {
           processRawData(tempDataFilePath);
       writeProcessedData(colorAgeLocationToCount);
     } catch (Exception e) {
-      System.out.println("ProcessingJob failed: " + e.getMessage());
+      logger.error("ProcessingJob failed: " + e.getMessage());
     }
   }
 
@@ -62,28 +66,28 @@ public class ProcessingJob {
   }
 
   public static String downloadRawData() throws IOException {
-    System.out.println("downloadRawData: start downloading data");
+    logger.info("downloadRawData: start downloading data");
 
     // Create temporary folder and file
     Path tempDir = Files.createTempDirectory("rawData");
     File tempDataFile = new File(tempDir.toFile(), "raw_data.csv");
-    System.out.println(
+    logger.info(
         "downloadRawData: processing from " + RAW_DATA_BUCKET + " to " + PROCESSED_DATA_BUCKET);
 
     // Read from Google Cloud Storage bucket
     Storage storage = StorageOptions.getDefaultInstance().getService();
     Blob blob = storage.get(RAW_DATA_BUCKET, RAW_DATA_FILE);
     blob.downloadTo(tempDataFile.toPath());
-    System.out.println("downloadRawData: downloaded data to " + tempDataFile.getAbsolutePath());
+    logger.info("downloadRawData: downloaded data to " + tempDataFile.getAbsolutePath());
     return tempDataFile.getAbsolutePath();
   }
 
   public static Dictionary<String, SquirrelSegment> processRawData(String tempDataFilePath)
       throws FileNotFoundException, IOException {
-    System.out.println("processRawData: start processing data");
+    logger.info("processRawData: start processing data");
     SquirrelCensusDictionaryBuildResult result =
         SquirrelCensusDictionaryBuilder.buildFromRawCsvFile(tempDataFilePath);
-    System.out.println(
+    logger.info(
         String.format(
             "processRawData: processed %s records, removed %s",
             result.numOfRowsProcessed, result.numOfRowsIgnored));
@@ -96,7 +100,7 @@ public class ProcessingJob {
    */
   public static void writeProcessedData(Dictionary<String, SquirrelSegment> colorAgeLocationToCount)
       throws UnsupportedEncodingException {
-    System.out.println("writeProcessedData: start writing");
+    logger.info("writeProcessedData: start writing");
     Gson gson = new Gson();
     Enumeration<String> keys = colorAgeLocationToCount.keys();
     Storage storage = StorageOptions.getDefaultInstance().getService();
@@ -110,6 +114,6 @@ public class ProcessingJob {
       byte[] jsonStringAsByes = jsonString.getBytes(utf8CharsetName);
       storage.create(cloudStorageBlobInfo, jsonStringAsByes, 0, jsonStringAsByes.length);
     }
-    System.out.println("writeProcessedData: wrote " + colorAgeLocationToCount.size() + " files");
+    logger.info("writeProcessedData: wrote " + colorAgeLocationToCount.size() + " files");
   }
 }
