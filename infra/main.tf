c38154e708a675c12dd7a712c761fd1bac01023e
Katie McLaughlin <katie@glasnt.com>
2024-05-14 09:39:14 +1000

    Update infra/main.tf
    Co-authored-by: Nim Jayawardena <i.am.nim.jay@gmail.com>

--- infra/main.tf
+++ infra/main.tf
@@ -17,7 +17,7 @@
 locals {
   unique = "${var.deployment_name}-${random_id.default.hex}"
 
-  application_image = "us-docker.pkg.dev/hsa-public/containers/cloud-client-api/${lower(var.language)}:${var.image_version}-pr93"
+  application_image = "us-docker.pkg.dev/hsa-public/containers/cloud-client-api/${lower(var.language)}:${var.image_version}"
 }
 
 resource "random_id" "default" {
